8e961ea27bff7fa57d1d2b3448ba0fba3e08bd51
NaveedGujral <naveedgujral93@gmail.com>
2024-04-25 11:28:07 +0100

    portfolio WIP

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        custom: {
+          grey: '#2e2f2f',
+          white: 
+            {
+              50:'#f7f9f9',
+              100:'#e8eded',
+              150:'#d0dcdc',
+            },       
+          blue: '#2397be',
+          red: '#e94d35',
+        }
+      }
     },
+    fontFamily: {
+      LexendGiga: ["LexendGiga","sans-serif"],
+    }
   },
   plugins: [],
 };
