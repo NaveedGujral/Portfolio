82c768ff221c97c34216334946af84d64f64e979
NaveedGujral <naveedgujral93@gmail.com>
2024-10-25 21:11:56 +0100

    smooth scrolling complete, Landing Anims Complete, About Anims WIP

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,8 +6,10 @@ import './globals.css';
 // components
 import Curves from "./Curves";
 import LandingTitle from "./components/SVG/LandingTitle";
-import SmoothScroll from "./components/SmoothScroll";
 import TypedComp from "./components/TypedComp";
+import Lenis from 'lenis'
+import { motion } from "framer-motion";
+import { easeInOut } from "framer-motion/dom";
 
 export default function Home() {
 
@@ -43,6 +45,20 @@ export default function Home() {
   }, [setScreenWidth, setScreenHeight]);
 
   useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis();
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+  }, []);
+
+  useEffect(() => {
     handleResize();
 
     if (window) {
@@ -67,30 +83,24 @@ export default function Home() {
 
   }, [loadingProgress]);
 
-  // useEffect(() => {
-  //   const typed1 = new Typed(typed1Ref.current, {
-  //     strings: ['sign', 'velop'],
-  //     typeSpeed: 150,
-  //     backSpeed: 150,
-  //     backDelay: 1000,
-  //     loop: true
-  //   })
-
-  //   return () => {
-  //     // Destroy Typed instance during cleanup to stop animation
-  //     typed1.destroy();
-  //   };
-  // }, [])
-
-
   return (
-    <main className="min-h-screen w-screen flex-col items-center justify-between p-0 overflow-x-hidden">
+    <main className="min-h-screen w-screen flex-col bg-custom-grey items-center justify-between p-0 overflow-x-hidden">
 
-      <section id="landing" className="flex bg-custom-grey w-screen h-screen top-0 justify-center items-center overflow-hidden">
-
-        {
-          contentLoaded ?
-            <div className='flex fixed justify-center items-center w-screen h-screen overflow-hidden'>
+      <section id="landing" className="flex w-screen h-screen top-0 justify-center items-center overflow-hidden">
+            <motion.div className='flex fixed justify-center items-center w-screen h-screen overflow-hidden'
+            initial={{
+              opacity: 0,
+              filter: 'blur(12px)'
+            }}
+            animate={{
+              opacity: 1,
+              filter: 'blur(0)'
+            }}
+            transition={{
+              duration: 2,
+              easings: easeInOut
+            }}
+            >
               <div className="w-full h-full flex  left-0">
                 <Curves screenWidth={screenWidth} screenHeight={screenHeight} className='z-0' />
               </div>
@@ -98,53 +108,43 @@ export default function Home() {
               <div className='absolute w-full h-full flex justify-center items-center p-10'>
                 <LandingTitle className="max-h-screen max-w-screen text-custom-white-50" />
               </div>
-            </div>
-
-            :
-            <div>
-              <h1 className=' font-JosefinSans text-custom-white-50 text-4xl'>Loading</h1>
-            </div>
-        }
-
+            </motion.div>
       </section>
 
-      <SmoothScroll>
-        <section id="about" className='relative top-[100vh] overflow-x-hidden flex flex-col w-screen min-h-screen items-center justify-center'>
-          <div className=' bg-gradient-to-t from-[#1a1a1a] to-transparent w-screen h-[50vh] '>
-          </div>
+      <section id="about" className='relative top-[0vh] overflow-x-hidden flex flex-col w-screen min-h-screen items-center justify-center'>
+        <div className=' bg-gradient-to-t from-[#1a1a1a] to-transparent w-screen h-[50vh] '>
+        </div>
 
 
-          <div id='intro' className='content-container bg-custom-grey'>
-            <div className='w-[1080px] m-20 flex justify-between'>
-              <div className='w-[352px] h-[352px] justify-center items-center flex relative'>
-                <img src={content[0].src} onLoad={() => handleContentLoad(content[0].id)} className='relative w-[348px] h-[348px] z-10 rounded-[10px]'></img>
-                <div className='gradBorderCore blur-[2px]' />
-                <div className='gradBorder blur-sm' />
-                <div className='gradBorder blur-md' />
-              </div>
-              <div className='w-[625px] h-[352px] flex flex-col justify-between'>
-                <TypedComp />
-                <h2 className="subHeading text-custom-white-50">
-                  I’m an industrial designer turned digital designer & developer.
-                </h2>
-                <p className="body text-custom-white-50">
-                  My training, experience and skillset as an industrial designer significantly overlaps with digital design and development.
-                  I love to work on projects that intersect creativity and technology.
-                </p>
-              </div>
+        <div id='intro' className='content-container bg-custom-grey'>
+          <div className='w-[1080px] m-20 flex justify-between'>
+            <div className='w-[352px] h-[352px] justify-center items-center flex relative'>
+              <img src={content[0].src} onLoad={() => handleContentLoad(content[0].id)} className='relative w-[348px] h-[348px] z-10 rounded-[10px]'></img>
+              <div className='gradBorderCore blur-[2px]' />
+              <div className='gradBorder blur-sm' />
+              <div className='gradBorder blur-md' />
+            </div>
+            <div className='w-[625px] h-[352px] flex flex-col justify-between'>
+              <TypedComp />
+              <h2 className="subHeading text-custom-white-50">
+                I’m an industrial designer turned digital designer & developer.
+              </h2>
+              <p className="body text-custom-white-50">
+                My training, experience and skillset as an industrial designer significantly overlaps with digital design and development.
+                I love to work on projects that intersect creativity and technology.
+              </p>
             </div>
           </div>
+        </div>
 
 
-          <div id='tech' className='content-container bg-blue-500'>
-          </div>
-          <div id='projects' className='content-container bg-green-500'>
-          </div>
-          <div id='footer' className='content-container'>
-          </div>
-        </section>
-      </SmoothScroll >
-
+        <div id='tech' className='content-container bg-blue-500'>
+        </div>
+        <div id='projects' className='content-container bg-green-500'>
+        </div>
+        <div id='footer' className='content-container'>
+        </div>
+      </section>
     </main >
   );
 }
