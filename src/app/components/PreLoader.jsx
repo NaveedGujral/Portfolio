467093b266ee34b948d60c6267b320f89244b43e
NaveedGujral <naveedgujral93@gmail.com>
2025-02-24 15:53:02 +0000

    quick loading screen complete

--- src/app/components/PreLoader.jsx
+++ src/app/components/PreLoader.jsx
@@ -11,19 +11,17 @@ import LandingTitle from "./SVG/LandingTitle";
 
 function PreLoader({ contentLoaded, loadingPercent }) {
   const [landing, setLanding] = useState(!contentLoaded);
-  const [isPresent, safeToRemove] = usePresence();
 
   useEffect(() => {
-    console.log(contentLoaded)
+    console.log(contentLoaded);
     setLanding(!contentLoaded);
-    contentLoaded && setTimeout(safeToRemove, 500); // Duration + Delay
   }, [contentLoaded]);
 
   return (
-    <AnimatePresence>
-      {isPresent && (
+    <AnimatePresence mode="wait">
+      {!contentLoaded && (
         <m.div
-          className="absolute w-full h-full flex justify-center items-center p-10 bg-purple-500 z-50"
+          className="absolute w-full h-full flex justify-center items-center p-10 bg-custom-grey z-50"
           initial={{
             opacity: 0,
             filter: "blur(12px)",
@@ -31,54 +29,28 @@ function PreLoader({ contentLoaded, loadingPercent }) {
           animate={{
             opacity: 1,
             filter: "blur(0)",
-            delay: 1,
-            duration: 2,
+            transition: {
+              duration: 1,
+              delay: 1,
+            },
           }}
           exit={{
             opacity: 0,
             filter: "blur(12px)",
-            delay: 1,
-            duration: 2,
+            transition: {
+              duration: 1,
+              delay: 1,
+            },
           }}
-        ></m.div>
+          onAnimationComplete={() => !contentLoaded}
+        >
+          {landing && (
+              <LandingTitle className="max-h-[50vh] max-w-[50vw]" />
+          )}
+        </m.div>
       )}
+      ;
     </AnimatePresence>
   );
-  // return (
-  //   <AnimatePresence>
-  //     {!contentLoaded && (
-  //       <m.div
-  //         className="absolute w-full h-full flex justify-center items-center p-10 bg-purple-500 z-50"
-  //         initial={{
-  //           opacity: 0,
-  //           filter: "blur(12px)",
-  //         }}
-  //         animate={{
-  //           opacity: 1,
-  //           filter: "blur(0px)",
-  //           delay: 1,
-  //           duration: 2,
-  //         }}
-  //         exit={{
-  //           opacity: 0,
-  //           filter: "blur(12px)",
-  //         }}
-  //       >
-  //         {/* {landing && (
-  //           <m.div
-  //             exit={{
-  //               opacity: 0,
-  //               filter: "blur(12px)",
-  //               delay: 1,
-  //               duration: 2,
-  //             }}
-  //           >
-  //             <LandingTitle className="max-h-[50vh] max-w-[50vw] " />
-  //           </m.div>
-  //         )} */}
-  //       </m.div>
-  //     )}
-  //   </AnimatePresence>
-  // );
 }
 export default PreLoader;
