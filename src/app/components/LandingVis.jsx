43f7d24e694d27649680927423b1f7bd3277be81
NaveedGujral <naveedgujral93@gmail.com>
2025-03-24 12:37:32 +0000

    responsive canvas complete

--- src/app/components/LandingVis.jsx
+++ src/app/components/LandingVis.jsx
@@ -15,16 +15,12 @@ void main() {
     worldPosition /= worldPosition.w;
     float vertexDistance = distance(vec3(0.0, 0.0, 0.0), worldPosition.xyz);
     float totalDistance = 31.5; 
-    float distanceAsPercentage = vertexDistance / totalDistance;
-    float indexLowerValue = floor(distanceAsPercentage * 256.0);
-    float indexHigherValue = ceil(distanceAsPercentage * 256.0);
-    float frequency = mix(u_frequencyArray[int(indexLowerValue)], u_frequencyArray[int(indexHigherValue)], fract(distanceAsPercentage));
     
     adjustedPosition = position;
-    adjustedPosition.y = frequency/ 15.0;
+    // adjustedPosition.y = frequency/ 15.0;
     // adjustedPosition.y = frequency / u_particleHeight;
-    gradientMix = (frequency / 256.0) - 0.125;
-    distanceAsMixValue = clamp(adjustedPosition.y, 0.0, 1.0);
+    // gradientMix = (frequency / 256.0) - 0.125;
+    distanceAsMixValue = clamp(adjustedPosition.y, 1.0, 1.0);
     gl_PointSize = 2.0;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(adjustedPosition, 1.0);
 }
@@ -38,20 +34,21 @@ varying float distanceAsMixValue;
 varying float gradientMix;
 
 void main() {
-    vec3 color = mix(vec3(u_colorA), vec3(u_colorB), gradientMix);
-    gl_FragColor = vec4(color, distanceAsMixValue);
-    // gl_FragColor = vec4(color, 1.0);
+    // vec3 color = mix(vec3(u_colorA), vec3(u_colorB), gradientMix);
+    // gl_FragColor = vec4(color, distanceAsMixValue);
+    gl_FragColor = vec4(u_colorA, 1.0);
 }
 `;
 
-export default function LandingVis({}) {
+export default function LandingVis({ width, height }) {
+
   const planeDistance = 31.5;
-  const planeGap = -0.5;
+  const planeGap = 0.5;
 
   const positions = useMemo(() => {
     const posArr = [];
-    for (let x = -planeDistance; x <= planeDistance; x += planeGap) {
-      for (let z = planeDistance; z >= -planeDistance; z -= planeGap) {
+    for (let x = -width/32; x <= width/32; x += planeGap) {
+      for (let z = -30; z <= 30; z += planeGap) {
         posArr.push(x, 0, z);
       }
     }
@@ -79,18 +76,22 @@ export default function LandingVis({}) {
             itemSize={3}
           />
         </bufferGeometry>
-        {/* <shaderMaterial
+        <shaderMaterial
           vertexShader={vertexShader}
           fragmentShader={fragmentShader}
           uniforms={uniforms}
           transparent={true}
-        /> */}
+        />
       </points>
     );
   }
   return (
-    <Canvas className="w-screen h-screen">
+    <Canvas
+      camera={{ position: [0, 100, 63], fov: 30, aspect: width / height }}
+      className= 'h-full w-full'
+    >
       <Plane />
+      <OrbitControls />
     </Canvas>
   );
 }
